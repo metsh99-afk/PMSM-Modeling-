3ab28132d5538d07649647da5f9f21e1372d6365
Mohamed <mhwork007@gmail.com>
2024-04-04 23:04:50 +0200

    Add Parameters

---
+++ Param1.m
@@ -0,0 +1,12 @@
+rs = 0.2;
+Lq = 300e-6;
+Ld = 300e-6;
+Lambda_m_r = 0.01;
+p = 2;
+
+We = 2*pi*10;
+Kp = We*Lq;
+Ki = We*rs;
+
+Lm = (2/3) * Lq;
+Ll = 0.01*272e-6;
---
+++ Param2.m
@@ -0,0 +1,6 @@
+%% PI Gains
+Gain_I  = 1;
+Gain_P = 1;
+%% Motor Param
+Ld = 0.16*10^(-3);
+Lq = 0.16*10^(-3);
